accd2bce924d2cfa0771385077f1bf4db974bf6e
ashish chincholkar <ashish_chincholkar@hotmail.com>
2024-02-16 23:05:45 -0500

    updated model jinja code

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Blog(models.Model):
     summary_header = models.CharField(max_length=100)
-    description = models.TextField(max_length=2000)
+    description = models.TextField(max_length=20000)
     date = models.DateField()
     #image = models.ImageField(upload_to="blog/images/")
 
